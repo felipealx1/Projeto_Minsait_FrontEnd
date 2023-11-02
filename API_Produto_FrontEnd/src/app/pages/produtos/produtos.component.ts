7f2c4db53c6e31c9cef25cd9d4dada96550941de
Jose Felipe <felipealexandre178@gmail.com>
2023-11-02 05:39:27 -0300

    diretorio produtos- Listar, altera e deletar os produtos exibidos da tabela

--- API_Produto_FrontEnd/src/app/pages/produtos/produtos.component.ts
+++ API_Produto_FrontEnd/src/app/pages/produtos/produtos.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Produto } from 'src/app/interfaces/produto';
+import { ProdutosService } from 'src/app/services/produtos.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-produtos',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent {
+  produtos: Produto[] = [];
+
+  constructor(private produtosService: ProdutosService){};
+
+  listarProdutos(): void{
+    this.produtosService.listarProdutos().subscribe(
+      retorno => this.produtos = retorno);
+  }
+
+  deletarProduto(id: number): void {
+    Swal.fire({
+      title: 'Confirmar exclusão',
+      text: 'Tem certeza de que deseja excluir este produto?',
+      icon: 'warning',
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.produtosService.deletarProduto(id).subscribe(() => {
+          this.listarProdutos();
+          Swal.fire('Produto excluído com sucesso', '', 'success');
+        });
+      }
+    });
+  }
+
+  atualizarProduto(produto: Produto): void {
+    this.produtosService.atualizarProduto(produto).subscribe(() => {
+      this.listarProdutos();
+      Swal.fire('Produto atualizado com sucesso', '', 'success');
+    });
+  }
+
+  ngOnInit(){
+    this.listarProdutos();
+  }
+  
 
 }
