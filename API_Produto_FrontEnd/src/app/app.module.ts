a811001f840ce8c891c1c5203ef44d0513f16db8
Jose Felipe <felipealexandre178@gmail.com>
2023-11-02 04:25:36 -0300

    Diretórios criado header, pages, services e interfaces

--- API_Produto_FrontEnd/src/app/app.module.ts
+++ API_Produto_FrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CadastrarProdutosComponent,
+    HomeComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
