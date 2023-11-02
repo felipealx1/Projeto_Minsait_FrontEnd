631b24a96d9e67e5ea2127b457a2eb483fbd9fe5
Jose Felipe <felipealexandre178@gmail.com>
2023-11-02 04:50:09 -0300

    impelementação das rotas e dos services em Produto.service

--- API_Produto_FrontEnd/src/app/app-routing.module.ts
+++ API_Produto_FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'produtos', component: ProdutosComponent
+  },
+  {
+    path: 'produtos/cadastrar', component: CadastrarProdutosComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- API_Produto_FrontEnd/src/app/services/produtos.service.ts
+++ API_Produto_FrontEnd/src/app/services/produtos.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../interfaces/produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
+  private url:string = 'http://localhost:8080/api/produtos';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  listarProdutos(): Observable<Produto[]>{
+    return this.http.get<Produto[]>(this.url);
+  }
+
+  cadastrarProduto(produto: Produto): Observable<Produto>{
+    return this.http.post<Produto>(this.url, produto);
+  }
+
+  editarProduto(produto: Produto): Observable<Produto>{
+    return this.http.put<Produto>(this.url, produto);
+  }
+
+  deletarProduto(id: number) {
+    const url = `${this.url}/${id}`;
+    return this.http.delete(url);
+  }
+
+  atualizarProduto(produto: Produto) {
+    const url = `${this.url}/${produto.id}`;
+    return this.http.put(url, produto);
+  }
+
 }
